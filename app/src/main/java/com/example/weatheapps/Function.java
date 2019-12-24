2cd52acc23e831e52174e7f46a064bfd5968c4a0
hermezs <maxkirillov8@gmail.com>
2019-12-24 20:28:09 +0300

    location
    Create button "Location"

--- app/src/main/java/com/example/weatheapps/Function.java
+++ app/src/main/java/com/example/weatheapps/Function.java
@@ -6,24 +6,107 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class Function {
-  // private static final String OPEN_WEATHER_MAP_API = "2b1cdf491ccf9192213ece92d3da79c1";
+
+
 
   public interface AsyncResponse {
-
     void processFinish(String output1, String output2, String output3, String output4, String output5, String output6, String output8);
+    void getLocation(String city);
   }
+
+  public static class Location extends AsyncTask<String,Void,JSONObject>{
+    public AsyncResponse del = null;
+    public Location(AsyncResponse as){
+      del=as;
+    }
+    @Override
+    protected JSONObject doInBackground(String... params) {
+      JSONObject jsonLoc = null;
+      try {
+        jsonLoc = getLocation();
+      } catch (Exception e) {
+        Log.d("Error", "Cannot process JSON results", e);
+      }
+      return jsonLoc;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject json) {
+      try {
+        if(json != null){
+          String city = json.getString("city");
+          del.getLocation(city);
+        }
+      } catch (JSONException e) {
+
+      }
+    }
+
+
+    public static JSONObject getLocation(){
+
+      URL obj = null;
+      try {
+        obj = new URL("http://api.ipstack.com/check?access_key=fc3e2a13f4fe42814bff600fac16ac9b");
+      } catch (MalformedURLException e3) {
+        e3.printStackTrace();
+      }
+      HttpURLConnection connection = null;
+      try {
+        connection = (HttpURLConnection) obj.openConnection();
+      } catch (IOException e3) {
+        e3.printStackTrace();
+      }
+
+      BufferedReader in = null;
+      try {
+        in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+      } catch (IOException e2) {
+        e2.printStackTrace();
+      }
+      String inputLine;
+      StringBuffer response = new StringBuffer();
+
+      try {
+        while ((inputLine = in.readLine()) != null) {
+          response.append(inputLine);
+        }
+      } catch (IOException e1) {
+        e1.printStackTrace();
+      }
+      try {
+        in.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      JSONObject data = null;
+      try {
+        data = new JSONObject(response.toString());
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+      return data;
+
+    }
+
+
+  }
+
 
   public static class placeIdTask extends AsyncTask<String, Void, JSONObject> {
 
-    public AsyncResponse delegate = null;//Call back interface
+    public AsyncResponse delegate = null;
 
     public placeIdTask(AsyncResponse asyncResponse) {
       delegate = asyncResponse;
@@ -39,6 +122,7 @@ public class Function {
       }
       return jsonWeather;
     }
+
 
     @Override
     protected void onPostExecute(JSONObject json) {
@@ -66,11 +150,9 @@ public class Function {
   public static JSONObject getWeatherJSON(String s,String met){
     try {
 
-      URL url = new URL(String.format("http://api.openweathermap.org/data/2.5/weather?q="+s+"&mode=json&APPID=eee0b2156b64eb1b3bd718f7a825bd97="+met));
+      URL url = new URL(String.format("http://api.openweathermap.org/data/2.5/weather?q="+s+"&mode=json&APPID=2b1cdf491ccf9192213ece92d3da79c1&units="+met));
       HttpURLConnection connection =
         (HttpURLConnection)url.openConnection();
-
-     // connection.addRequestProperty("x-api-key", OPEN_WEATHER_MAP_API);
 
       BufferedReader reader = new BufferedReader(
         new InputStreamReader(connection.getInputStream()));
@@ -86,7 +168,6 @@ public class Function {
       if(data.getInt("cod") != 200){
         return null;
       }
-
       return data;
     }catch(Exception e){
       return null;
--- app/src/main/java/com/example/weatheapps/MainActivity.java
+++ app/src/main/java/com/example/weatheapps/MainActivity.java
@@ -9,13 +9,14 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
   String city;
   String met = "metric";
   TextView weather;
   CheckBox c,f,k;
   EditText cityin;
-  Button subuton;
+  Button subuton,location;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +28,27 @@ public class MainActivity extends AppCompatActivity {
     cityin = (EditText)findViewById(R.id.City);
     c = (CheckBox)findViewById(R.id.Cels);
     f = (CheckBox)findViewById(R.id.Fahre);
-    k = (CheckBox)findViewById(R.id.Kelvin);
     subuton = (Button)findViewById(R.id.button);
+    location = (Button)findViewById(R.id.location);
+
+    location.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Function.Location asyncTask =new Function.Location(new Function.AsyncResponse() {
+          @Override
+          public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn, String sun_rise) {
+
+          }
+
+          @Override
+          public void getLocation(String city) {
+            cityin.setText(city);
+          }
+
+        });
+        asyncTask.execute();
+      }
+    });
 
     subuton.setOnClickListener(new View.OnClickListener(){
       @Override
@@ -43,13 +63,19 @@ public class MainActivity extends AppCompatActivity {
           met="default";
         }
         Function.placeIdTask asyncTask =new Function.placeIdTask(new Function.AsyncResponse() {
+         @Override
           public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn, String sun_rise) {
             weather.setText(weather_temperature);
           }
+
+          @Override
+          public void getLocation(String city) {
+
+          }
+
         });
         asyncTask.execute(city,met);
       }
     });
-
   }
 }
